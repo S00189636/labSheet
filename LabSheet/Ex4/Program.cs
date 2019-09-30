1418d54c3b674166d2a9c77a41e10c317e6b320d
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2019-09-30 19:12:37 +0100

    Ex4 done

---
+++ LabSheet/Ex4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister register1 = new CashRegister("CR1");
+            CashRegister register2 = new CashRegister();
+
+            register1.AddItem(58.5);
+            register1.AddItem(5.9);
+            register1.AddItem(5.5);
+            register1.AddItem(18);
+            register1.AddItem(5.59);
+
+            register2.AddItem(52.5);
+            register2.AddItem(15.5);
+            register2.AddItem(53.5);
+            register2.AddItem(544.5);
+
+            Console.WriteLine();
+            PrintReport(register1);
+
+            Console.WriteLine();
+            register2.Name = "CR2";
+            PrintReport(register2);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintReport(CashRegister register)
+        {
+            Console.WriteLine("Total cash in {0} is: {1} ", register.Name, register.GetTotalCash());
+            Console.WriteLine("Total Items count in {0} is: {1} ", register.Name, register.GetTotalItems());
+        }
+    }
+}
