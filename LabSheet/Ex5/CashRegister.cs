68ecfcc7779d7f7261e4fec84ec96b7b46404da6
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2019-09-30 19:46:02 +0100

    Ex5 done

---
+++ LabSheet/Ex5/CashRegister.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class CashRegister
+    {
+        private int totlaItems;
+        private double totalCash;
+        public string Name { get; set; }
+
+        public CashRegister()
+        {
+            totalCash = 0;
+            totlaItems = 0;
+            Name = "No Name CR";
+        }
+        public CashRegister(string name)
+        {
+            totalCash = 0;
+            totalCash = 0.0;
+            Name = name;
+        }
+
+        public void AddItem(double price)
+        {
+            Console.WriteLine("Adding an Item worth {0:C} to the {1}", price, Name);
+            totlaItems++;
+            totalCash += price;
+        }
+
+        public double GetTotalCash()
+        {
+            return totalCash;
+        }
+
+        public string GetTotalItems()
+        {
+            return totlaItems.ToString();
+        }
+
+    }
+}
---
+++ LabSheet/Ex5/Store.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class Store
+    {
+        private List<CashRegister> cashRegisters;
+        private string totlaItems
+        {
+            get
+            {
+                int total = 0;
+                foreach (CashRegister cr in cashRegisters)
+                {
+                    total += int.Parse(cr.GetTotalItems());
+                }
+                return total.ToString();
+            }
+        }
+        private string totalCash
+        {
+            get
+            {
+                double total = 0;
+                foreach (CashRegister cr in cashRegisters)
+                {
+                    total += cr.GetTotalCash();
+                }
+                return total.ToString();
+            }
+        }
+
+        public Store() { cashRegisters = new List<CashRegister>(); }
+        public Store(CashRegister cashRegister)
+        {
+            cashRegisters = new List<CashRegister>();
+            AddCashRegister(cashRegister);
+        }
+
+        public void AddCashRegister(CashRegister cashRegister)
+        {
+            cashRegisters.Add(cashRegister);
+        }
+
+        public void PrintReport()
+        {
+            Console.WriteLine("Total cash in {0} Cash registers : {1} ", cashRegisters.Count ,totalCash);
+            Console.WriteLine("Total Items count in {0} Cash registers is: {1} ", cashRegisters.Count, totlaItems);
+        }
+    }
+}
