d3ff04cff3c3e251f9b1c962acc5066ea8b5d6e6
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2019-10-03 18:54:02 +0100

    Finished main

---
+++ LabSheet/SchoolsDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemo
+{
+    class Program
+    {
+        static List<School> schools = new List<School>();
+        static void Main(string[] args)
+        {
+            int schoolsCount = 1;
+            schoolsCount = int.Parse(GetInput("how many schools - (min 1 , max 99+) ?"));
+            for (int i = 0; i < schoolsCount; i++)
+            {
+                string input = null;
+                School school = new School();
+                input = GetInput("school name: ");
+                school.SetName(input);
+                input = GetInput("Enrolled students number in "+ school.GetName()+":");
+                school.SetNumber(int.Parse(input));
+                schools.Add(school);
+            }
+            Console.WriteLine("You have added {0} schools", schools.Count);
+            schools.Sort((s1, s2) => s1.GetName().CompareTo(s2.GetName()));
+            Console.WriteLine("Schools sorted");
+            Console.WriteLine();
+            PrintSchools();
+            Console.ReadLine();
+        }
+
+        private static string GetInput(string message)
+        {
+            Console.Write("Please enter {0} ", message);
+            return Console.ReadLine();
+        }
+
+        static public void PrintSchools()
+        {
+            foreach (School item in schools)
+            {
+                Console.WriteLine("{0} has {1} students", item.GetName(), item.GetEnrolledStudents());
+            }
+        }
+    }
+}
---
+++ LabSheet/SchoolsDemo/School.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemo
+{
+    class School
+    {
+        private string Name;
+        private int EnrolledStudents;
+
+        public School() { }
+        public School(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetEnrolledStudents()
+        {
+            return EnrolledStudents;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetNumber(int number)
+        {
+            EnrolledStudents = number;
+        }
+    }
+}
